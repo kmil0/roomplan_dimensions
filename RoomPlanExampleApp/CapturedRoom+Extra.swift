55441d16ec612e260f3736737b8bf41e83d623c4
camilo <camil0_andr3s@hotmail.com>
2023-05-18 11:08:49 -0400

    fork roomplan

---
+++ RoomPlanExampleApp/CapturedRoom+Extra.swift
@@ -0,0 +1,142 @@
+//
+//  CapturedRoom+Extra.swift
+//  rp2
+//
+//  Created by Mark Kim on 6/16/22.
+//
+
+import RoomPlan
+import UIKit
+
+extension CapturedRoom.Object.Category: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .storage:
+            return "Storage"
+        case .refrigerator:
+            return "Refrigerator"
+        case .stove:
+            return "Stove"
+        case .bed:
+            return "Bed"
+        case .sink:
+            return "Sink"
+        case .washerDryer:
+            return "Washer"
+        case .toilet:
+            return "Toilet"
+        case .bathtub:
+            return "Bathtub"
+        case .oven:
+            return "Oven"
+        case .dishwasher:
+            return "Dishwasher"
+        case .table:
+            return "Table"
+        case .sofa:
+            return "Sofa"
+        case .chair:
+            return "Chair"
+        case .fireplace:
+            return "Fireplace"
+        case .television:
+            return "Screen"
+        case .stairs:
+            return "Stairs"
+        @unknown default:
+            return "Default"
+        }
+    }
+}
+
+func text(for category: CapturedRoom.Object.Category) -> String {
+    return "\(category)"
+}
+
+func category(for text: String) -> CapturedRoom.Object.Category {
+    switch text {
+    case "Storage":
+        return .storage
+    case "Refrigerator":
+        return .refrigerator
+    case "Stove":
+        return .stove
+    case "Bed":
+        return .bed
+    case "Sink":
+        return .sink
+    case "Washer":
+        return .washerDryer
+    case "Toilet":
+        return .toilet
+    case "Bathtub":
+        return .bathtub
+    case "Oven":
+        return .oven
+    case "Dishwasher":
+        return .dishwasher
+    case "Table":
+        return .table
+    case "Sofa":
+        return .sofa
+    case "Chair":
+        return .chair
+    case "Fireplace":
+        return .fireplace
+    case "Screen":
+        return .television
+    case "Stairs":
+        return .stairs
+    default:
+        return .storage
+    }
+}
+
+func color(for category: CapturedRoom.Object.Category) -> UIColor {
+    var color: UIColor
+
+    switch category {
+    case .storage:
+        color = UIColor.white
+    case .refrigerator:
+        color = UIColor.white
+    case .stove:
+        color = UIColor.white
+    case .bed:
+        color = UIColor.white
+    case .sink:
+        color = UIColor.systemBlue
+    case .washerDryer:
+        color = UIColor.systemBlue
+    case .toilet:
+        color = UIColor.systemBlue
+    case .bathtub:
+        color = UIColor.systemBlue
+    case .oven:
+        color = UIColor.white
+    case .dishwasher:
+        color = UIColor.white
+    case .table:
+        color = UIColor.white
+    case .sofa:
+        color = UIColor.white
+    case .chair:
+        color = UIColor.white
+    case .fireplace:
+        color = UIColor.red
+    case .television:
+        color = UIColor.white
+    case .stairs:
+        color = UIColor.white
+    @unknown default:
+        color = UIColor.white
+        break
+    }
+
+    return color
+}
+
+func capturedRoomObjectCategoryStrings() -> [String] {
+    return ["Unknown", "Storage", "Refrigerator", "Stove", "Bed", "Sink", "Washer", "Toilet", "Bathtub", "Oven", "Dishwasher", "Table", "Sofa", "Chair", "Fireplace", "Screen", "Stairs"]
+}
+
